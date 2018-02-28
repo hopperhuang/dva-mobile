131c030f6cd33011d74ac221858cf5e01c179497
黄家炜 <huangjiawei@innotechx.com>
2018-02-28 17:19:41 +0800

    add

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -4,12 +4,23 @@ import { Button } from 'antd-mobile';
 import styles from './IndexPage.less';
 
 
+// const CustomIcon = ({ type, className = '', size = 'md', ...restProps }) => (
+//   <svg
+//     className={`am-icon am-icon-${type.substr(1)} am-icon-${size} ${className}`}
+//     {...restProps}
+//   >
+//     <use xlinkHref={type} /> {/* svg-sprite-loader@0.3.x */}
+//     {/* <use xlinkHref={#${type.default.id}} /> */} {/* svg-sprite-loader@latest */}
+//   </svg>
+//    );
+
 function IndexPage() {
   return (
     <div className={styles.normal}>
       <div className={styles.test} >
         <Button>这里是一个button</Button>
         <div className={styles.another} >这里是另外一个区域</div>
+
       </div>
     </div>
   );
