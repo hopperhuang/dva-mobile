432688b568f6be449cdf41475de8c430f9f1fcd4
黄家炜 <huangjiawei@innotechx.com>
2018-05-01 00:04:08 +0800

    add

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-
+console.log(211)
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
